e3e6b67c109012741cd96085a3c79b2660ada708
Jisan Jung <jjjung13@gmail.com>
2020-07-25 12:53:46 -0400

    trending movies, when Results component is loaded

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,7 +6,7 @@ export class Item extends Component {
         return (
             <div onClick={this.props.setDetails} style={{ width: "300px", height: "450px"}} className="mb-5 m-3 mt-5">
 
-                <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} className="h-100 w-100" style={{ objectFit: "cover" }}/>
+                <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} className="h-100 w-100" style={{ objectFit: "cover" }} alt="Movie"/>
 
                 <div className="flex flex-between p-2">
                     <h5>{movie.title}</h5>
--- src/components/Results.js
+++ src/components/Results.js
@@ -10,17 +10,16 @@ export class Results extends Component {
         super();
         this.state = {
             query: "",
+            endpoint: "https://api.themoviedb.org/3/trending/all/day?api_key=b2768da444ca6ca13d3a6180597e85f0",
             movies: [],
             details: [],
             error: ""
         }
     }
 
-    // handle event when form is submitted
-    handleSubmit(e) {
-        e.preventDefault();
-
-        axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=b2768da444ca6ca13d3a6180597e85f0&query=${this.state.query}`)
+    // trending movies
+    componentDidMount() {
+        axios.get(this.state.endpoint)
             .then(res => {
                 this.setState({
                     movies: [...res.data.results]
@@ -33,6 +32,31 @@ export class Results extends Component {
                     console.log(this.state.error)
                 });
             });
+    }
+
+    // handle event when form is submitted
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let newEndpoint = `https://api.themoviedb.org/3/search/movie/?api_key=b2768da444ca6ca13d3a6180597e85f0&query=${this.state.query}`;
+
+        this.setState({
+            endpoint: newEndpoint
+        }, () => {
+            axios.get(this.state.endpoint)
+            .then(res => {
+                this.setState({
+                    movies: [...res.data.results]
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                }, () => {
+                    console.log(this.state.error)
+                });
+            });
+        });
     }
 
     // handle input change
